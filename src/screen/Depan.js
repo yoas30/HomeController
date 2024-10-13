961e20c05fa8ad124f8ecc98f1c8996e2cb33bfc
yoas30 <yoas.rinatan30@gmail.com>
2024-10-13 15:58:59 +0700

    modified:   android/app/build.gradle 	modified:   package-lock.json 	modified:   src/screen/Depan.js 	new file:   src/screen/styles.js

--- src/screen/Depan.js
+++ src/screen/Depan.js
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image, Platform, Alert, ScrollView } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image, Alert, ScrollView } from 'react-native'
 import { useState, useEffect, React } from "react";
 import Paho from "paho-mqtt";
+
+import styles from './styles'; // Path sesuai dengan lokasi file styles.js
 
 
 
@@ -9,8 +11,6 @@ const logo = require('../.././img/iot.png');
 const off = require('../.././img/off.png');
 const on = require('../.././img/on.png');
 
-
-const textColor12 = 'red';
 
 client = new Paho.Client(
   "broker.mqtt-dashboard.com",
@@ -22,7 +22,12 @@ const DashboardDepan = () => {
 
   //const [value, setValue] = useState(0); //ok
   const [textKonek, setTextKonek] = useState ('LOADING...');
-  const textColor = textKonek === 'LOADING...' ? 'red' : 'green';
+  const textColor = textKonek === 'LOADING...' 
+  ? 'red' 
+  : textKonek === 'TERKONEKSI !!' 
+  ? 'green' 
+  : 'yellow'; // Warna default atau kondisi tambahan
+  
   //const fontFamily1 = Platform.OS === 'ios' ? 'Oswald-Bold' : 'Oswald-SemiBold'; 
 
 
@@ -135,7 +140,6 @@ function SemuaHidup(c) {
   return (
     
   <View style={styles.container1}>
-    <ScrollView showsVerticalScrollIndicator={false} > 
         <View style={styles.boxKecil}>
             <Image source={logo} style={{ width: 35, height: 35, marginTop: 20, marginLeft: 20, marginRight: 5 }} />
             <Text style={{ 
@@ -146,178 +150,71 @@ function SemuaHidup(c) {
           }}
           > HOME IOT</Text>
       </View>
-
-      <Image source={slide1} style={styles.slideGambar} />
-
+      <View style={styles.containerRow}>
+            <Image source={slide1} style={styles.slideGambar} />
+      </View>
               <Text style={{ 
                   fontSize : 28,
                   color : "#FFF",
-                  marginTop : 40,
+                  marginTop : 10,
                   fontFamily : 'Oswald-Bold',
                   textAlign: "center"
               }} > KELUARGA CANTIK DAN GANTENG </Text>
 
       <Text style={[styles.textStatus, { color: textColor }]}> {textKonek} </Text>
-      
 
         <View style={ styles.box }>
-          <ScrollView> 
-            <View style={ styles.containerColumn}>
-         
-                    <Text style={ styles.title }>Pompa Air</Text>
-                      <View style={ styles.containerRow}>
-                          <TouchableOpacity style={styles.tombolKlik} onPress={() => { pompaHidup(client); } } >
-                              <Image source={on} style={styles.tombolGambar}/>
-                          </TouchableOpacity>
-                          <TouchableOpacity style={styles.tombolKlik} onPress={() => { pompaMati(client); } } >
-                            <Image source={off} style={styles.tombolGambar}/>
-                          </TouchableOpacity>  
-                      </View>  
+              <ScrollView showsVerticalScrollIndicator={false} > 
+                    <View style={ styles.containerColumn}>
+                        <Text style={ styles.title }>Pompa Air</Text>
+                              <TouchableOpacity style={styles.tombolKlik} onPress={() => { pompaHidup(client); } } >
+                                  <Text style={styles.subtitle}>Hidupkan</Text>
+                              </TouchableOpacity>
+                              <TouchableOpacity style={styles.tombolKlikMerah} onPress={() => { pompaMati(client); } } >
+                                  <Text style={styles.subtitle}>Matikan</Text>
+                              </TouchableOpacity>   
 
-                    <Text style={ styles.title }> Lampu Dapur </Text>
-                      <View style={ styles.containerRow}>
-                          <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuDapurHidup(client); }} >
-                              <Image source={on} style={styles.tombolGambar}/>
-                          </TouchableOpacity>
-                          <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuDapurMati(client);} } >
-                              <Image source={off} style={styles.tombolGambar}/>
-                          </TouchableOpacity>
-                      </View> 
+                        <Text style={ styles.title }> Lampu Dapur </Text>
+                              <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuDapurHidup(client); }} >
+                                  <Text style={styles.subtitle}>Hidupkan</Text>
+                              </TouchableOpacity>
+                              <TouchableOpacity style={styles.tombolKlikMerah} onPress={() => { LampuDapurMati(client);} } >
+                                  <Text style={styles.subtitle}>Matikan</Text>
+                              </TouchableOpacity>
 
-                    <Text style={ styles.title }> Lampu Belakang </Text>
-                      <View style={ styles.containerRow}>
-                          <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuBelakangHidup(client); }} >
-                              <Image source={on} style={styles.tombolGambar}/>
-                          </TouchableOpacity>
-                          <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuBelakangMati(client);} } >
-                              <Image source={off} style={styles.tombolGambar}/>
-                          </TouchableOpacity>
-                      </View> 
+                        <Text style={ styles.title }> Lampu Belakang </Text>
+                              <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuBelakangHidup(client); }} >
+                                  <Text style={styles.subtitle}>Hidupkan</Text>
+                              </TouchableOpacity>
+                              <TouchableOpacity style={styles.tombolKlikMerah} onPress={() => { LampuBelakangMati(client);} } >
+                                  <Text style={styles.subtitle}>Matikan</Text>
+                              </TouchableOpacity>
 
-                    <Text style={ styles.title }> Lampu Belakang </Text>
-                      <View style={ styles.containerRow}>
-                          <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuDepanHidup(client); }} >
-                              <Image source={on} style={styles.tombolGambar}/>
-                          </TouchableOpacity>
-                        <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuDepanMati(client);} } >
-                              <Image source={off} style={styles.tombolGambar}/>
-                        </TouchableOpacity>
-                      </View> 
-
-                    <Text style={ [styles.title, { color: "#066A4E", fontSize: 22 }]}> SEMUA PERALATAN LISTRIK</Text>
-                      <View style={styles.containerColumn}>
-                          <TouchableOpacity style={styles.tombolKlikTerakhir} onPress={() => { SemuaHidup(client); }} >
-                              <Image source={on} style={[styles.tombolGambar, { width: 40, height: 40 }]}/>
-                          </TouchableOpacity>
-                        <TouchableOpacity style={styles.tombolKlikTerakhir} onPress={() => { SemuaMati(client);} } >
-                             <Image source={off} style={[styles.tombolGambar, { width: 40, height: 40 }]}/>
-                        </TouchableOpacity>
-                      </View> 
-            </View>    
-          </ScrollView> 
-        </View>
-    </ScrollView>
+                        <Text style={ styles.title }> Lampu Depan </Text>
+                              <TouchableOpacity style={styles.tombolKlik} onPress={() => { LampuDepanHidup(client); }} >
+                                  <Text style={styles.subtitle}>Hidupkan</Text>
+                              </TouchableOpacity>
+                              <TouchableOpacity style={styles.tombolKlikMerah} onPress={() => { LampuDepanMati(client);} } >
+                                    <Text style={styles.subtitle}>Matikan</Text>
+                              </TouchableOpacity>
+                                    <View style={styles.containerColumn}>
+                                            <Text style={ [styles.title, { color: "#990e11", fontSize: 22, flexWrap: 'wrap', lineHeight: 24, marginTop: 35, marginBottom: 20 }]}
+                                        >MATIKAN SEMUA PERALATAN LISTRIK</Text>
+                                                    <TouchableOpacity style={[styles.tombolKlik, { height: 60}]} onPress={() => { SemuaHidup(client); }} >
+                                                          <Text Text style={styles.subtitle}>Hidupkan</Text>
+                                                    </TouchableOpacity>
+                                                    <TouchableOpacity style={[styles.tombolKlikMerah, { height: 60 }]} onPress={() => { SemuaMati(client);} } >
+                                                            <Text style={styles.subtitle}>Matikan</Text>
+                                                    </TouchableOpacity> 
+                                    </View> 
+                    </View>    
+              </ScrollView>                  
+          </View> 
+          <Text style={ [styles.title, { color: 'white', fontSize: 17, flexWrap: 'wrap', lineHeight: 24, marginTop: 20, marginBottom: 20, textAlign: 'center', justifyContent: 'flex-end' }]}
+                              >🌿🏠💕 Home Controller 💕🏠🌿</Text>
   </View>
   )
 }
-
-
-const styles = StyleSheet.create({
-
-  container1: {
-                flex:1,
-                backgroundColor:"black"
-  },
-  slideGambar:{
-                width: 340,         // Lebar gambar untuk landscape
-                height: 175,        // Tinggi gambar untuk landscape
-                borderRadius: 20,   // Membuat pojokan melengkung
-                marginLeft: 20,   // Jarak sisi kiri
-                marginRight: 20,  // Jarak sisi kanan
-                marginTop: 50,    // Jarak dari bagian atas layar
-                alignContent: 'center',
-                alignItems: 'center'
-  },
-  tombolGambar: {
-                width: 30,          // Sesuaikan ukuran gambar
-                height: 30,         // Sesuaikan ukuran gambar
-  },
-  box: {
-                marginTop: 20,
-                width: '100%',              // Lebar box
-                height: 350,               // Tinggi box
-                backgroundColor: 'rgba(240, 240, 240, 0.1)', // Warna latar belakang transparan
-                borderRadius: 15,          // Membuat pojok kotak melengkung
-                elevation: 5,              // Efek bayangan (untuk Android)
-                shadowColor: '#000',       // Warna bayangan (untuk iOS)
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.3,
-                shadowRadius: 4,
-                flexDirection: 'row', 
-                justifyContent: 'space-around'
-  },
-  scrollContent: {
-                flexDirection: 'column', // Mengatur konten agar vertikal
-                padding: 10,             // Opsional: Memberi padding di dalam ScrollView
-  },
-  boxKecil:{
-                flexDirection: 'row', 
-                justifyContent: 'space-around',
-                alignSelf: 'flex-start'
-  },
-  containerRow: {
-                flexDirection: 'row',      // Mengatur tombol dalam baris
-                justifyContent: 'center', // Memberikan jarak antar tombol
-                marginBottom: 10,          // Jarak antar kelompok tombol
-                justifyContent: 'space-around'
-  },
-  containerColumn: {
-                flexDirection: 'column',      // Mengatur tombol dalam kolom
-                marginLeft:'1%',
-                marginRight: '1%'
-  },
-  title: {
-                fontFamily : 'Changa-Regular',
-                fontWeight : 300,
-                marginTop : 10,
-                marginBottom : 10,
-                textAlign: "center",
-                fontSize: 20,             // Ukuran teks
-                fontWeight: 'bold',       // Teks tebal
-                color: 'white',            // Warna teks
-                marginBottom: 15,         // Jarak bawah
-  },
-  subtitle: {
-                fontFamily : 'Oswald-Bold',
-                color: '#fff',               // Warna teks
-                fontSize: 10,                // Ukuran teks yang cukup besar dan terbaca
-                fontWeight: 'bold',          // Teks tebal
-  },
-  tombolKlik: {
-                backgroundColor: "#314555",
-                width: 150,               // Sesuaikan ukuran tombol
-                height: 80,               // Sesuaikan ukuran tombol
-                borderRadius: 10,         // Setengah dari width/height agar berbentuk lingkaran
-                justifyContent: 'center', // Menempatkan konten di tengah secara vertikal
-                alignItems: 'center',     // Menempatkan konten di tengah secara horizontal
-                marginTop: 10,
-  },
-  tombolKlikTerakhir: {
-              backgroundColor: "#314555",
-              width: "auto",              // Sesuaikan ukuran tombol
-              height: 100,                 // Sesuaikan ukuran tombol
-              borderRadius: 10,           // Setengah dari width/height agar berbentuk lingkaran
-              justifyContent: 'center',   // Menempatkan konten di tengah secara vertikal
-              alignItems: 'center',       // Menempatkan konten di tengah secara horizontal
-              marginTop: 10,
-},
-  textStatus: {
-                fontSize : 20,
-                marginTop : 5,
-                fontFamily : 'Changa-Regular',
-                textAlign: "center"
-  },
-});
 
 export default DashboardDepan;
 
---
+++ src/screen/styles.js
@@ -0,0 +1,115 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+    container1: {
+                  flex:1,
+                  backgroundColor:'black'
+    },
+    slideGambar:{
+                  width: 420,         // Lebar gambar untuk landscape
+                  height: 185,        // Tinggi gambar untuk landscape
+                  borderRadius: 20,   // Membuat pojokan melengkung
+                  marginTop: 50,    // Jarak dari bagian atas layar
+                  alignContent: 'center',
+                  alignItems: 'center',
+                  resizeMode: 'repeat'
+    },
+    tombolGambar: {
+                  width: 30,          // Sesuaikan ukuran gambar
+                  height: 30,         // Sesuaikan ukuran gambar
+    },
+    box: {        
+                  flex: 1,                    // Tinggi box
+                  flexDirection: 'column',
+                  marginTop: 20,
+                  width: '100%',              // Lebar box
+                  backgroundColor: 'rgba(240, 240, 240, 0)', // Warna latar belakang transparan
+                  borderRadius: 15,          // Membuat pojok kotak melengkung
+                //   elevation: 5,              // Efek bayangan (untuk Android)
+                //   shadowColor: '#000',       // Warna bayangan (untuk iOS)
+                //   shadowOffset: { width: 0, height: 2 },
+                //   shadowOpacity: 0.3,
+                //   shadowRadius: 4,
+                  justifyContent: 'space-around',
+                  overflow: 'hidden',
+    },
+    scrollContent: {
+                  flexDirection: 'column', // Mengatur konten agar vertikal
+                  padding: 10,             // Opsional: Memberi padding di dalam ScrollView
+    },
+    boxKecil:{
+                  flexDirection: 'row', 
+                  justifyContent: 'space-around',
+                  alignSelf: 'flex-start'
+    },
+    containerRow: {
+                  flexDirection: 'row',      // Mengatur tombol dalam baris
+                  justifyContent: 'center', // Memberikan jarak antar tombol
+                  marginBottom: 10,          // Jarak antar kelompok tombol
+                  justifyContent: 'space-evenly'
+    },
+    containerColumn: {
+                  flexDirection: 'column',      // Mengatur tombol dalam kolom
+                  marginLeft:'1%',
+                  marginRight: '1%'
+    },
+    title: {
+                  fontFamily : 'Oswald-Bold',
+                  fontWeight : 300,
+                  marginTop : 10,
+                  marginBottom : 10,
+                  textAlign: 'center',
+                  fontSize: 20,                 // Ukuran teks
+                  fontWeight: 'bold',           // Teks tebal
+                  color: 'white',               // Warna teks
+                  marginBottom: 15,             // Jarak bawah
+                  textTransform: 'uppercase'    // Mengubah teks menjadi huruf besar semua
+    },
+    subtitle: {
+                  fontFamily : 'Oswald-Bold',
+                  color: '#fff',               // Warna teks
+                  fontSize: 20,                // Ukuran teks yang cukup besar dan terbaca
+                  fontWeight: 'bold',          // Teks tebal
+    },
+    tombolKlik: {
+                  backgroundColor: '#018b7c',
+                  width: 400,               // Sesuaikan ukuran tombol
+                  height: 70,               // Sesuaikan ukuran tombol
+                  borderRadius: 30,         // Setengah dari width/height agar berbentuk lingkaran
+                  justifyContent: 'center', // Menempatkan konten di tengah secara vertikal
+                  alignItems: 'center',     // Menempatkan konten di tengah secara horizontal
+                  marginTop: 0,
+                  marginBottom: 10,
+                  alignSelf: 'center'
+    },
+    tombolKlikMerah: {
+                 backgroundColor: '#990e11',
+                 width: 400,               // Sesuaikan ukuran tombol
+                 height: 70,               // Sesuaikan ukuran tombol
+                 borderRadius: 30,         // Setengah dari width/height agar berbentuk lingkaran
+                 justifyContent: 'center', // Menempatkan konten di tengah secara vertikal
+                 alignItems: 'center',     // Menempatkan konten di tengah secara horizontal
+                 marginTop: 0,
+                 marginBottom: 10,
+                 alignSelf: 'center'
+},
+    tombolKlikTerakhir: {
+                backgroundColor: '#314555',
+                width: 'auto',              // Sesuaikan ukuran tombol
+                height: 100,                // Sesuaikan ukuran tombol
+                borderRadius: 20,           // Setengah dari width/height agar berbentuk lingkaran
+                justifyContent: 'center',   // Menempatkan konten di tengah secara vertikal
+                alignItems: 'center',       // Menempatkan konten di tengah secara horizontal
+                marginTop: 10,
+  },
+    textStatus: {
+                  fontSize : 20,
+                  marginTop : 5,
+                  fontFamily : 'Changa-Regular',
+                  textAlign: "center"
+    },
+  });
+  
+
+export default styles;
